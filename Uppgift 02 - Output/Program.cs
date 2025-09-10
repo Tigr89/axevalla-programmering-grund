6c056eab86af11cf4717789199ffa8428574fb41
Axevalla <sspel25adni@axevalla.fhsk.se>
2025-09-10 11:37:11 +0200

    hej

---
+++ Uppgift 02 - Output/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace output___Adam_N
+{
+    internal class Program
+    {
+        public static bool Hello { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+       
+    } 
+}
