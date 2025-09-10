158048c1a9a7f09b9bffa221605767b295476eb4
Meedown <ht24phso@axevalla.fhsk.se>
2025-09-10 10:29:56 +0200

    wow cool assignments

---
+++ Uppgift 02 - Output/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("1\n 2");
+Console.ReadLine();
+
+Console.WriteLine("    4\n     5");
+Console.ReadLine();
+
+Console.WriteLine("       7\n        8");
+Console.ReadLine();
+
+Console.WriteLine("i cant be bothered to keep this going lol.\nwriting newlines without being able to use the numpad for backslashes is painful.");
+Console.ReadLine();
+
+Console.WriteLine("i mean come on, who even uses ctrl+alt for special characters anymore?!!");
+Console.ReadLine();
+
+Console.WriteLine("(i do. i use ctrl+alt to write the or statement symbol thing ||\nbut for whatever reason i just find alt+92 on numpad to be more comfortable for backslashes)");
+Console.ReadLine();
+
+Console.WriteLine("ah fuck i just realized it was because numlock was disabled. so i actually can use the numpad to write backslashes.");
+Console.ReadLine();
