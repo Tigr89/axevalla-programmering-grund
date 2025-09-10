c8581e7a97ba2843e54f9efeccc9d586b5083b97
rymd-spel <sspel25eddu@axevalla.fhsk.se>
2025-09-10 11:55:16 +0200

    uppgifte 2

---
+++ uppgift 3 som jag ska göra/uppgift 3 den riktiga delen för mig/uppgift 3 den riktiga delen för mig/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_3_den_riktiga_delen_för_mig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello world");
+
+        }
+    }   
+
+        
+
+
+}
