719c3d81b12637b5a481a0de58508efeab273515
Lucas Klevvall <ht24kllu@axevalla.fhsk.se>
2025-09-17 11:19:36 +0200

    test

---
+++ Uppgift 03 - Operander och beräkningar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int current_year = 2025;
+
+            Console.WriteLine("Välkommen till programmet");
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine();
+
+
+            Console.WriteLine("Vilket år är du född?");
+            int birth_year = Convert.ToInt32(Console.ReadLine());
+
+            int age = current_year - birth_year;
+
+            Console.WriteLine("Du heter: " + namn);
+            Console.WriteLine("Du är : " + age + "år");
+
+
+        }
+    }
+}
