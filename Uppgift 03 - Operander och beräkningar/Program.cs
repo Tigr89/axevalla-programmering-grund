7bde5b39e85870f92e765ee07438b40732483e0d
rymd-spel <sspel25eddu@axevalla.fhsk.se>
2025-09-17 12:02:17 +0200

    uppgift 3 färdig

---
+++ Uppgift 03 - Operander och beräkningar/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lejjandro_beräkning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            int current_year = 2025;
+            int birth_year;
+//Welcome to user
+            Console.WriteLine("Welcome user");
+            Console.ReadLine();
+//User name.           
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+//User age.
+            Console.WriteLine("What year are you born?");
+            birth_year = Convert.ToInt32(Console.ReadLine());
+//message to user with name and age.            
+            Console.WriteLine(name + " you are " + (age = current_year - birth_year) + " years old");
+
+            Console.WriteLine(("It was good to knwon a little bit about you ") + name + " have a nice day.");
+        }
+    }
+}
