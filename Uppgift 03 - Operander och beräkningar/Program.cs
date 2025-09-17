7e4808e2065ef66ba1b6d5d66aa861ff343e6c57
david.s <sspel25dasa@axevalla.fhsk.se>
2025-09-17 10:52:18 +0200

    uppgift 3

---
+++ Uppgift 03 - Operander och beräkningar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            int currentyear = 2025;
+            int yearofbirth;
+            Console.WriteLine("Welcome. Please write down your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Write down your year of birth:");
+            yearofbirth = Convert.ToInt32(Console.ReadLine());
+            age = currentyear - yearofbirth;
+            Console.WriteLine("Your name is " + name + " and you are " + age + " years old.");
+
+        }
+    }
+}
