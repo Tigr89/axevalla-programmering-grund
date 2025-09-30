c0a150dbde6959e010037e1407df4949e3e60c2a
david.s <sspel25dasa@axevalla.fhsk.se>
2025-09-30 11:24:11 +0200

    switch

---
+++ Uppgift 06 - Switch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 100;
+            string fruit;
+            while (money > 0)
+            {
+                Console.WriteLine("Pick a fruit!");
+                fruit = Console.ReadLine();
+                switch(fruit)
+                {
+                    case "apple":
+                        money = money - 10;
+                        Console.WriteLine("Paid 10 money");
+                        break;
+                    case "orange":
+                        money = money - 20;
+                        Console.WriteLine("Paid 20 money");
+                        break;
+                    case "banana":
+                        money = money - 30;
+                        Console.WriteLine("Paid 30 money");
+                        break;
+                    default:
+                        Console.WriteLine("Bad answer");
+                        break;
+                }
+            }
+            Console.WriteLine("You are out of money");
+        }
+    }
+}
