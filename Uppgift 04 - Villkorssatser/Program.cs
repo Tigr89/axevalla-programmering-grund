c651e2d6b425b5493d75882fad0f06c84ec87ccc
david.s <sspel25dasa@axevalla.fhsk.se>
2025-09-30 10:18:46 +0200

    if then else

---
+++ Uppgift 04 - Villkorssatser/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfThenElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            Console.WriteLine("Who are you?");
+            name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age > 70)
+            {
+                Console.WriteLine("Your name is " + name + " and you are an old fart.");
+            }
+            else if (age > 40)
+            {
+                Console.WriteLine("Your name is " + name + " and your life is wasting away");
+            }
+            else if (age > 18)
+            {
+                Console.WriteLine("Your name is " + name + " and you are just getting started");
+            }
+            else
+            Console.WriteLine("Your name is " + name + " and you are still too young");
+
+        }
+    }
+}
