9748574fcb97b82d4d964af5dd651f9c35da188c
Axevalla <sspel25rory@axevalla.fhsk.se>
2025-10-01 11:52:35 +0200

    pågående uppgift 4

---
+++ Uppgift 04 - Villkorssatser/uppgift 4/uppgift 4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace uppgift_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what's your name?");
+
+            string name = "Adam";
+            int age;
+
+            int birth_year = 2005;
+
+            int current_year = 2025;
+
+
+
+
+
+
+            name = Console.ReadLine();
+            Console.WriteLine("Hej " + name + " när är du född?");
+
+
+
+            birth_year = Convert.ToInt32(Console.ReadLine());
+            age = current_year - birth_year;
+
+            Console.WriteLine("Du är " + age + " år gammal");
+
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 12)
+            {
+                Console.WriteLine(name + " är ett barn");
+            }
+
+
+        }
+    }
+}
