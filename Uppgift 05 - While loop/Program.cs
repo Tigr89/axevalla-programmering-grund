b77e30034fd93111f5a2699b014c14ee219f953f
James-0-0 <vt25joab@axevalla.fhsk.se>
2025-10-01 11:49:20 +0200

    misc

---
+++ Uppgift 05 - While loop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int saldo = 100;
+            int val = 0;
+
+            while (saldo > 0)
+            {
+                Console.WriteLine("Vilken glasss vill du ha, 1, 2 eller 3?");
+                val = Convert.ToInt32(Console.ReadLine());
+                if (val == 1)
+                {
+                    saldo -= 10;
+                }
+                else if (val == 2)
+                {
+                    saldo  -= 20;
+                }
+                else if (val == 3)
+                {
+                    saldo -= 30;
+                }
+            }
+            Console.WriteLine("Slut på Pengar!");
+        }
+    }
+}
