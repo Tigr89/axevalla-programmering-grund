b4e6ab22bfd02364bff864d1fc8a912fa3691669
James-0-0 <vt25joab@axevalla.fhsk.se>
2025-10-01 10:51:21 +0200

    maise

---
+++ Uppgift 04 - Villkorssatser/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int current_year = 2025;
+            int birth_year;
+            Console.WriteLine("Hi, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please state your age.");
+            birth_year = Convert.ToInt32(Console.ReadLine());
+            int age = birth_year;
+            if (age < 12)
+            {
+                Console.WriteLine(age + " You are a child " + name);
+            }
+            else if (age < 30)
+            {
+                Console.WriteLine(age + " You are a young adult " + name);
+            }
+            else if (age < 50)
+            {
+                Console.WriteLine(age + " You are middle aged " + name);
+            }
+            else if (age > 50)
+            {
+                Console.WriteLine(age + " You are old " + name);
+            }
+        }
+    }
+}
