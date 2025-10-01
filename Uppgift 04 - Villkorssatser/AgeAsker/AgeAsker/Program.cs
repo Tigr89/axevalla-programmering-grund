a8b7c0d34e50a0d7a4827f7fdbb2b350a673409b
Axevalla <sspel25rory@axevalla.fhsk.se>
2025-10-01 11:33:58 +0200

    upgift 4, oct 1

--- Uppgift 04 - Villkorssatser/AgeAsker/AgeAsker/Program.cs
+++ Uppgift 04 - Villkorssatser/AgeAsker/AgeAsker/Program.cs
@@ -10,9 +10,33 @@ namespace AgeAsker
     {
         static void Main(string[] args)
         {
-            int name;
-            int age;
+            int age = 0;
+            string name;
 
+            Console.WriteLine("När var du född? (Svara i nummer)");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vad heter du?");
+            name = Console.ReadLine();
+            age = DateTime.Now.Year - age;
+            Console.WriteLine("Hej " + name + " du är " + age + " eller " + (age - 1) + " år gammal");
+
+           
+            if (age < 0 || age > 120)
+            {
+                Console.WriteLine("Du måste ange ett rimligt årtal");
+            }
+            else if (age < 24)
+            {
+                Console.WriteLine("Du är en bäbis");
+            }
+            else if (age >= 23 && age < 25)
+            {
+                Console.WriteLine("Du är en helt rimlig ålder :)");
+            }
+            else
+            {
+                Console.WriteLine("Du är uråldrig");
+            }
         }
     }
 }
