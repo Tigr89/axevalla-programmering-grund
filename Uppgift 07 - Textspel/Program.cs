c1600adace9442bd19d5c110e594ec64d10e1820
James-0-0 <vt25joab@axevalla.fhsk.se>
2025-10-10 08:46:03 +0200

    misc

---
+++ Uppgift 07 - Textspel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName = "";
+            int playerHP = 100;
+            string weaponType = "";
+            int weaponChoice;
+            int playerDamage = 10;
+            int pMaxDamage = playerDamage * 2;
+            int pMinDamage;
+            int enemyHP = 100;
+            int eMaxDamage = 10;
+            int eMinDamage = 5;
+            int enemyDamage;
+        }
+    }
+}
