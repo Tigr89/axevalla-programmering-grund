e9e53896dbf91c820faf02f2c42520367db62273
Axevalla <sspel25rory@axevalla.fhsk.se>
2025-10-15 11:57:04 +0200

    snart klar med uppgift 5

--- Uppgift 05 - While loop/uppgift 5 - Adam Nilsson/uppgift 5 - Adam Nilsson/Program.cs
+++ Uppgift 05 - While loop/uppgift 5 - Adam Nilsson/uppgift 5 - Adam Nilsson/Program.cs
@@ -12,10 +12,48 @@ namespace uppgift_5___Adam_Nilsson
         static void Main(string[] args)
         {
             int saldo = 100;
+            int val = 0;
+
+
             while (saldo > 0)
             {
-                Console.WriteLine(saldo);
-                saldo++;
+                Console.WriteLine("Du har " + saldo + " kvar. Vilken glass vill du köpa? Skriv 1 för Piggelin som kostar 10kr. Skriv 2 för Magnum som kostar 20kr. Skriv 3 för Dajmglass för 30kr");
+                
+
+                val = Convert.ToInt32(Console.ReadLine());
+
+                if (val == 1);
+                {
+                    saldo = saldo - 10;
+
+                    Console.WriteLine("Du köpte Piggelin. Du har " + saldo + " kvar");
+                }
+
+                if (val == 2)
+                {
+                    saldo = saldo - 20;
+
+                    Console.WriteLine("Du köpte Magnum. Du har " + saldo + " kvar");
+
+                }
+;
+
+                if (val == 3) 
+                {
+                    saldo = saldo - 30;
+
+                    Console.WriteLine("Du köpte Dajmglass. Du har " + saldo + " kvar");
+                }
+                //Berätta för användaren vad den har för valmöjligheter
+                //Använd Console.WriteLine();
+
+                //Lagra användarens val i "val"-variabeln
+
+                //Kolla  vad spelaren valde och dra pengar från dess saldo.
+
+
+
+                //Om spelaren har pengar kvar så kommer programmet loopa här!
             }
         }
     }
