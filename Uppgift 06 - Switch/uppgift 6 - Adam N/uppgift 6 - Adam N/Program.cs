c4e2e9311f1f51619977f9408c1ea38bc7f2949b
Axevalla <sspel25rory@axevalla.fhsk.se>
2025-11-05 11:57:04 +0100

    uppgift 7 klar

--- Uppgift 06 - Switch/uppgift 6 - Adam N/uppgift 6 - Adam N/Program.cs
+++ Uppgift 06 - Switch/uppgift 6 - Adam N/uppgift 6 - Adam N/Program.cs
@@ -1,21 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace uppgift_6___Adam_N
 
 {
-    string money;
+
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            switch (money)
-            { 
-            case 1
+            int saldo = 100;
+            string money;
+            int val = 0;
+
+            while (saldo > 0)
+
+            {
+                Console.WriteLine("Du har " + saldo + " kvar. Vilken glass vill du köpa? Skriv 1 för Piggelin som kostar 10kr. Skriv 2 för Magnum som kostar 20kr. Skriv 3 för Dajmglass för 30kr");
+
+
+                
+
+                while (val <= 0 || val >= 4)
+                {
+                    val = Convert.ToInt32(Console.ReadLine());
+
+                    switch (val)
+                    {
+                        case 1:
+                            saldo = saldo - 10;
+                            Console.WriteLine("Du köpte Piggelin. Du har  " + saldo + "kvar ");
+                            break;
+
+
+
+                        case 2:
+                            saldo = saldo - 20;
+                            Console.WriteLine("Du köpte Magnum. Du har  " + saldo + "kvar ");
+                            break;
+
+
+
+                        case 3:
+                            saldo = saldo - 30;
+                            Console.WriteLine("Du köpte Dajmglass. Du har  " + saldo + "kvar ");
+                            break;
+
+                        default:
+
+                            Console.WriteLine("går ej, välj 1, 2 eller 3");
+
+                            val = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
+
+                }
+                    val = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
             }
+
+
+
+
+
         }
     }
 }
