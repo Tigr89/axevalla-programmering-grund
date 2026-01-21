e41b29f4ac985b431ebfe0965f8bdfb78f495936
Pancake <sspel25adni@axevalla.fhsk.se>
2026-01-21 11:46:24 +0100

    uppgift 8 klar!

--- Uppgift 08 - Sten, sax, påse/stenSaxPåse - Adam N/stenSaxPåse - Adam N/Program.cs
+++ Uppgift 08 - Sten, sax, påse/stenSaxPåse - Adam N/stenSaxPåse - Adam N/Program.cs
@@ -16,67 +16,117 @@ namespace stenSaxPåse___Adam_N
             int psaldo = 100;
             int esaldo = 100;
             int val = 0;
-            string choice;
+            string Playerchoice ="";
             string Rock;
             string Paper;
             string Scissors;
             Random EnemyRNG = new Random();
             int EnemyChoice;
             string EnemyAttack;
-            int bet;
+            int bet = 0;
             EnemyAttack  = "";
 
-            while (psaldo + esaldo > 0)
+            while (psaldo > 0 && esaldo > 0)
             {
                 Console.WriteLine("it's time to play Rock, Paper Scissors. How much do you wanna bet? You currently have " + psaldo + "kr and the enemy has " + esaldo + "kr");
-                rockbet = Convert.ToInt32(Console.ReadLine());
+                bet = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("You bet " + bet + "kr. Let's begin. You now have to Choose Rock Paper or Scissors and win over the opponent"); 
+                Console.WriteLine("You bet " + bet + "kr. Let's begin. You now have to Choose Rock Paper or Scissors and win over the opponent");
 
-            choice = Console.ReadLine();
-            if (choice.ToLower() == "rock")
-            {
-                Console.WriteLine("you choose " + choice);
-            }
-
-            
-            if (choice.ToLower() == "paper")
-            {
-                Console.WriteLine("you choose " + choice);
-            }
-
-            
-            if (choice.ToLower() == "scissors")
-            {
-                Console.WriteLine("you choose " + choice);
-            }
-            
-
-            
+                Playerchoice = Console.ReadLine();
+                if (Playerchoice.ToLower() == "rock")
+                {
+                    Console.WriteLine("you choose " + Playerchoice);
+                }
 
 
-            EnemyChoice = EnemyRNG.Next(1, 4);
-            if (EnemyChoice == 1)
-            {
-                EnemyAttack = "Rock";
-            }
-            else if (EnemyChoice == 2)
-            {
-                EnemyAttack = "Paper";
-            }
-            else if (EnemyChoice == 3)
-            {
-                EnemyAttack = "Scissors";
-            }
+                if (Playerchoice.ToLower() == "paper")
+                {
+                    Console.WriteLine("you choose " + Playerchoice);
+                }
+
+
+                if (Playerchoice.ToLower() == "scissors")
+                {
+                    Console.WriteLine("you choose " + Playerchoice);
+                }
+
+
+
+
+
+                EnemyChoice = EnemyRNG.Next(1, 4);
+                if (EnemyChoice == 1)
+                {
+                    EnemyAttack = "Rock";
+                }
+                else if (EnemyChoice == 2)
+                {
+                    EnemyAttack = "Paper";
+                }
+                else if (EnemyChoice == 3)
+                {
+                    EnemyAttack = "Scissors";
+                }
 
                 Console.WriteLine("Enemy choose " + EnemyAttack);
-            }
-            //NEXT LESSION I HAVE TO MAKE HOW YOU CAN WIN AND LOSE
-         //  if EnemyAttack == rock && choice == Rock
-            {
 
+
+                if (EnemyAttack == Playerchoice)
+                {
+                    Console.WriteLine("Tie. Try again");
+                }
+                else if (EnemyAttack == "Rock" && Playerchoice.ToLower() == "paper")
+                {
+                    psaldo = (psaldo + bet);
+                    esaldo = (esaldo - bet);
+                    Console.WriteLine("You Win. You have " + psaldo + "kr left and enemy has " + esaldo + "kr left");
+                }
+
+
+                else if (EnemyAttack == "Paper" && Playerchoice.ToLower() == "scissors")
+                {
+                    psaldo = (psaldo + bet);
+                    esaldo = (esaldo - bet);
+                    Console.WriteLine("You Win. You have " + psaldo + "kr left and enemy has " + esaldo + "kr left");
+                }
+
+                else if (EnemyAttack == "Scissors" && Playerchoice.ToLower() == "rock")
+                {
+                    psaldo = (psaldo + bet);
+                    esaldo = (esaldo - bet);
+                    Console.WriteLine("You Win. You have " + psaldo + "kr left and enemy has " + esaldo + "kr left");
+                }
+
+                else if (EnemyAttack == "Rock" && Playerchoice.ToLower() == "scissors")
+                {
+                    psaldo = (psaldo - bet);
+                    esaldo = (esaldo + bet);
+                    Console.WriteLine("You lost. You have " + psaldo + "kr left and enemy has " + esaldo + "kr left");
+                }
+
+                else if (EnemyAttack == "Paper" && Playerchoice.ToLower() == "rock")
+                {
+                    psaldo = (psaldo - bet);
+                    esaldo = (esaldo + bet);
+                    Console.WriteLine("You lost. You have " + psaldo + "kr left and enemy has " + esaldo + "kr left");
+                }
+                else if (EnemyAttack == "Scissors" && Playerchoice.ToLower() == "paper")
+                {
+                    psaldo = (psaldo - bet);
+                    esaldo = (esaldo + bet);
+                    Console.WriteLine("You lost. You have " + psaldo + "kr left and enemy has " + esaldo + "kr left");
+                }
+            }
+            if (psaldo < 0)
+            {
+                Console.WriteLine("Sorry loser. You lost...");
+            }
+            else if (esaldo < 0)
+            {
+                Console.WriteLine("Congrats! You won!");
             }
         }
-               //saldo - bet at the end of the script
+
     }
 }
