40238a08631c636dc96344a027c0da9976c0d540
unknown <h24emwe@axevalla.fhsk.se>
2026-01-21 10:34:23 +0100

    Uppgift 4 och 5

---
+++ Uppgift 04 - Villkorssatser/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emiluppg5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int saldo = 100;
+            int val = 0;
+            
+            while (saldo > 0)
+            {
+                Console.WriteLine("välj mellan glass 1 för 10kr, 2 för 20kr och 3 för 30kr");
+                val = Convert.ToInt32(Console.ReadLine());
+                if (val == 1)
+                {
+                    saldo = saldo - 10;
+                    Console.WriteLine("tack, du har " + saldo + " kr kvar");
+                      
+                }
+                else if (val == 2 && saldo >= 20)
+                {
+                    saldo = saldo - 20;
+                    Console.WriteLine("tack, du har " + saldo + " kr kvar");
+                }
+                else if (val ==3 && saldo >= 30)
+                {
+                    saldo = saldo - 30;
+                    Console.WriteLine("tack, du har " + saldo + " kr kvar");
+
+                }
+                else
+                {
+                    
+                    Console.WriteLine("du har inte råd du har bara " + saldo + " kr!!!!!!");
+
+                   
+                }
+
+                if (val > 3)
+                {
+                    Console.WriteLine("finns inget sånt här");
+
+                }
+                if (val<1) { Console.WriteLine("finns inget sånt här");
+                   
+                 }
+                if (saldo <= 0)
+                {
+                    Console.WriteLine("du e pank stick härifrån!!");
+                    Console.ReadLine();
+
+
+
+                }
+            }
+            
+        }
+    }
+}
---
+++ Uppgift 05 - While loop/while loop/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string[] s = { "stena", "sax", "påse" };
+
+for (int i = 0; i < s.Length; i++)
+{
+    Console.WriteLine(s[i]);
+}
+
+
+
+
+
+
+var rand = new Random();
+Console.WriteLine("hej nu spelar vi sten, sax, påse" );
+int enemy = rand.Next(1,4);
+string y = Console.ReadLine();
+if (y == "sten"){
+    if (enemy == 1)
+    {
+        Console.WriteLine("oavgjort");
+    }
+    if (enemy == 2)
+    {
+        Console.WriteLine("victory");
+    }
+    if (enemy == 3)
+    {
+        Console.WriteLine("defeat");
+    }   
+}
+if (y == "sax") 
+{
+    if (enemy == 2)
+    {
+        Console.WriteLine("oavgjort");
+    }
+    if (enemy == 3)
+    {
+        Console.WriteLine("victory");
+    }
+    if (enemy == 1)
+    {
+        Console.WriteLine("defeat");
+    }
+}
+if (y == "påse")
+{
+    if (enemy == 3)
+    {
+        Console.WriteLine("oavgjort");
+    }
+    if (enemy == 1)
+    {
+        Console.WriteLine("victory");
+    }
+    if (enemy == 2)
+    {
+        Console.WriteLine("defeat");
+    }
+}
+
