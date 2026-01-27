5284fe606f45758b7e7ea252f10d957aa41ddd52
Philip <ht24phso@axevalla.fhsk.se>
2026-01-27 11:40:42 +0100

    done

--- Uppgift 05 - While loop/while/obj/Debug/net8.0/while.AssemblyInfo.cs
+++ Uppgift 05 - While loop/while/obj/Debug/net8.0/while.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("while")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0493af7a520ce60c0f28475b1951d8cc68eeb985")]
 [assembly: System.Reflection.AssemblyProductAttribute("while")]
 [assembly: System.Reflection.AssemblyTitleAttribute("while")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Uppgift 12 - Skriva till- och från fil/FileManagement/Program.cs
+++ Uppgift 12 - Skriva till- och från fil/FileManagement/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace FileManagement
@@ -12,27 +9,42 @@ namespace FileManagement
         static void Main(string[] args)
         {
             //Hämta information ifrån textfilen! Denna fil finns lokalt på datorn.
-            string input01 = File.ReadAllText("../../Tim's mapp/File00.txt");
-            //string input02 = ??? (ReadAllText)
-            //string[] input03 = ??? (ReadAllLines)
-            //string[] input04 = ??? (ReadAllLines)
+            string input01 = File.ReadAllText("../../FilesToRead/File01.txt");
+            string input02 = File.ReadAllText("../../FilesToRead/File02.txt");
+            string[] input03 = File.ReadAllLines("../../FilesToRead/File03.txt");
+            string[] input04 = File.ReadAllLines("../../FilesToRead/File04.txt");
+            string[] input05 = File.ReadAllLines("../../FilesToRead/File05.txt");
 
+         
             //Jobba med File01 här
             string result01 = File01Task(input01);
+            Console.WriteLine("Result 01:");
             Console.WriteLine(result01);
 
             //Jobba med File02 här
-            //Kalla på funktion
+            List<int> result02 = File02Task(input02);
+            Console.WriteLine("Result 02:");
+            foreach (int i in result02)
+            {
+                Console.WriteLine(i);
+            }
 
             //Jobba med File03 här
-            //Kalla på funktion
-
+            int[] result03 = File03Task(input03);
+            Console.WriteLine("Result 03:");
+            foreach (int i in result03)
+            {
+                Console.WriteLine(i);
+            }
             //Jobba med File04 här
-            //Kalla på funktion
+            string[] result04 = File04Task(input04);
             //Skriv till textdokumentet
+            File.WriteAllLines("../../FilesToRead/File04.txt", result04);
 
             //Jobba med File05 här
+            string[] result05 = File05Task(input05);
             //Kalla på funktion
+            File.WriteAllLines("../../FilesToRead/File05.txt", result05);
             //Skriv till textdokumentet
         }
 
@@ -43,13 +55,15 @@ namespace FileManagement
             char[] listToSort = input.ToCharArray();
             string returnString = "";
 
-            foreach(char letter in listToSort)
+            foreach (char letter in listToSort)
             {
                 returnString += letter;
             }
 
             //sortera arrayen här.
             //???
+            Array.Sort(listToSort);
+            returnString = new String(listToSort);
 
             //Här skickar vi tillbaka listan i datatypen "string".
             return returnString;
@@ -64,12 +78,13 @@ namespace FileManagement
             //vi kommer att vilja lägga till saker dynamiskt. 
             List<int> intsInList = new List<int>();
 
-            foreach(string siffra in listToSort)
+            foreach (string siffra in listToSort)
             {
                 //För varje (for each) siffra (vårt valda variabelnamn) i vår lista
-                //ska läggas in i vår lista "intsInList". 
+                //ska läggas in i vår lista "intsInList".
+                intsInList.Add(int.Parse(siffra));
             }
-
+            intsInList.Sort();
             return intsInList;
         }
 
@@ -77,31 +92,103 @@ namespace FileManagement
         {
             //Här får ni en färdig array med rader som enbart består av "bokstavssiffror".
             //Ert uppdrag är att sortera arrayen och skicka tillbaka den till main.
-
+            List<int> intsInList = new List<int>();
             //Börja med att konvertera varje array element till en siffra och spara i en List<int>.
-
-            //Sortera listan!
-
+            for (int i = 0; i < input.Length; i++)
+            {
+                intsInList.Add(int.Parse(input[i]));
+            }
+            intsInList.Sort();
+            int[] returnInts = intsInList.ToArray();
             //Returnera listan!
             //Ersätt null med ert resultat!
-            return null;
+            return returnInts;
         }
 
-        static void File04Task(string[] input)
+        static string[] File04Task(string[] input)
         {
-            //Svara på frågorna! 
+ 
+            string userName = "Me";
+            string birthYear = "1405";
+            string favoriteColor = "rgb(215, 58, 253)";
+            string favoriteBuilding = "Idfk";
+
+            string[] returnStrings = new string[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                string answer = "";
+                switch (i)
+                {
+                    case 0:
+                        answer = userName;
+                        break;
+                    
+                    case 1:
+                        answer = birthYear;
+                        break;
+
+                    case 2:
+                        answer = favoriteColor;
+                        break;
+
+                    case 3:
+                        answer = favoriteBuilding;
+                        break;
+                }
+                returnStrings[i] = input[i] + answer;
+                Console.WriteLine(returnStrings[i]);
+            }
+            return returnStrings;
             //Ledtråd: jobba med varje array-element för sig.
-            
-            
+
+
             //return? Just nu är funktionen "void" -- dvs. funktionen skickar inte tillbaka 
             //något värde.
             //Vill ni skicka tillbaka en string eller en List<string?/char?>?
         }
 
-        static void File05Task(string[] input)
+        static string[] File05Task(string[] input)
         {
-            //Samma uppgift som File04, fast nu ska ni ersätta den existerande informationen
-            //med ny information!
+            List<string> emptiedFields = new List<string>();
+            foreach (string s in input)
+            {
+                string currentField = s;
+                string[] splitString = currentField.Split(':');
+                splitString[0] += ": ";
+                emptiedFields.Add(splitString[0]);
+            }
+
+            string userName = "You";
+            string birthYear = "1852";
+            string favoriteColor = "rgb(153, 217, 14)";
+            string favoriteBuilding = "Not Mässen";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string answer = "";
+                switch (i)
+                {
+                    case 0:
+                        answer = userName;
+                        break;
+
+                    case 1:
+                        answer = birthYear;
+                        break;
+
+                    case 2:
+                        answer = favoriteColor;
+                        break;
+
+                    case 3:
+                        answer = favoriteBuilding;
+                        break;
+                }
+                emptiedFields[i] += answer;
+                Console.WriteLine(emptiedFields[i]);
+            }
+            string[] filledFields = emptiedFields.ToArray();
+            return filledFields;
         }
     }
 }
