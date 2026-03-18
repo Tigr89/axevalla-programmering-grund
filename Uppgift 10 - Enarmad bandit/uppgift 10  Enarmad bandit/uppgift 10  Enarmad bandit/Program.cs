cfceeca8d536786052d89852c207f88e28502ec7
Pancake <sspel25adni@axevalla.fhsk.se>
2026-03-18 11:56:34 +0100

    fijetgrjio

---
+++ Uppgift 10 - Enarmad bandit/uppgift 10  Enarmad bandit/uppgift 10  Enarmad bandit/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_10__Enarmad_bandit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            bool spin;
+            Random rnd = new Random();
+            int spin1 = rnd.Next(1, 5);
+            int spin2 = rnd.Next(1, 5);
+            int spin3 = rnd.Next(1, 5);
+
+
+            Console.WriteLine("How much money do you wanna bet on the slot machine?");
+
+            money = int.Parse(Console.ReadLine());
+
+            //make slotmachine spin
+
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
