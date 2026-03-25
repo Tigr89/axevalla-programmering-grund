8ba0edc6c79375a707ae1edf4dbcf4271b5f8f6e
Pancake <sspel25adni@axevalla.fhsk.se>
2026-03-25 11:57:09 +0100

    kof

--- Uppgift 10 - Enarmad bandit/uppgift 10  Enarmad bandit/uppgift 10  Enarmad bandit/Program.cs
+++ Uppgift 10 - Enarmad bandit/uppgift 10  Enarmad bandit/uppgift 10  Enarmad bandit/Program.cs
@@ -10,37 +10,39 @@ namespace uppgift_10__Enarmad_bandit
     {
         static void Main(string[] args)
         {
-            int money;
+            int coins;
+            int coinsinserted;
             bool spin;
             Random rnd = new Random();
-            int spin1 = rnd.Next(1, 5);
-            int spin2 = rnd.Next(1, 5);
-            int spin3 = rnd.Next(1, 5);
+            int spin1 = rnd.Next(1, 7);
+            int spin2 = rnd.Next(1, 7);
+            int spin3 = rnd.Next(1, 7);
+            int jackpot;
+            int winst;
+            string[] symbols = new[] { "seven", "Banana", "apples" }; 
+
+            Console.WriteLine("How many coins do you wanna bet on the slot machine?");
+
+            coins = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("You bet " + coins + " coins. Ready to gamble? Let's start!");
 
 
-            Console.WriteLine("How much money do you wanna bet on the slot machine?");
-
-            money = int.Parse(Console.ReadLine());
 
             //make slotmachine spin
+            void slotmachine()
+            {
+                if (spin1 == 7 && spin2 == 7 && spin3 == 7)
+                {
+                    coins = coinsinserted * 10;
+                }
 
-            
-            
+                else if (spin1 == /***random number that isn't matching with the other 2***/ && spin2 == 7 && spin3 == 7)
+                {
 
+                }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
 
 
 
