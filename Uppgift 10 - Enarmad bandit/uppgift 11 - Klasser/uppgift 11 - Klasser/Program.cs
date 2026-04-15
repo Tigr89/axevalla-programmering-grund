437e7c94a2bdc4b7558dfe31f71f1524827f2cc4
Pancake <sspel25adni@axevalla.fhsk.se>
2026-04-15 11:52:49 +0200

    oekweojipekfkor

---
+++ Uppgift 10 - Enarmad bandit/uppgift 11 - Klasser/uppgift 11 - Klasser/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_11___Klasser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> name;
+
+            {
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
