6d2bc9cda512b28653d806cbb356d1acccb4f651
Tigr89 <123071532+Tigr89@users.noreply.github.com>
2026-05-05 21:25:58 +0200

    La till bonusuppgift: fixdacode

---
+++ Bonus/FixDaCode/FixDaCode/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixDaCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //VARIABLER
+            string[] nameArray = new string[5];
+            int userChoice;
+
+            //Be användaren fylla listan med namn.
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Insert name :" + i);
+                nameArray[i] = Console.ReadLine();
+            }
+
+            //Här startar programloopen. 
+            while (true)
+            {
+                //Berätta för användaren vad de har för alternativ
+                Console.WriteLine("Vad vill du göra?");
+                Console.WriteLine("1. Lista alla namn");
+                Console.WriteLine("2. Lista specifik individ");
+                Console.WriteLine("3. Avsluta program");
+
+                userChoice = Console.ReadLine();
+
+                //Om användaren väljer ett icke-legitimt val, be dem välja igen.
+                while(userChoice < 1 || userChoice > 3)
+                {
+                    Console.WriteLine("ERROR. Välj en annan siffra");
+                    userChoice = Console.ReadLine();
+                }
+
+                //Skriv ut alla individer på separata rader
+                if(userChoice == 1)
+                {
+                    for(int i = 0; i < 5; i++)
+                    {
+                        Console.Write(nameArray[1]);
+                    }
+                }
+                
+                //Låt användaren välja en specifik individ och skriv sedan ut den.
+                if(userChoice == 2)
+                {
+                    Console.WriteLine("Välj en siffra?");
+
+                    userChoice = Console.ReadLine();
+
+                    for(int i = 0; i < 3; i++)
+                    {
+                        Console.WriteLine("ERROR. Välj en annan siffra");
+                        userChoice = Console.ReadLine();
+                    }
+
+                    //Skriv ut det som användaren har valt!
+                    Console.WriteLine(nameArray[int o = 1]);
+                }
+
+                //Avsluta programmet
+                if(userChoice == 3)
+                {
+                    Console.WriteLine("OK! Ha en trevlig dag!");
+                }
+            }
+        }
+    }
+}
