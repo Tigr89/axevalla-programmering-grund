bf855d0492b6cab90d41f845b2cb4ec8a5bc4a43
Pancake <sspel25adni@axevalla.fhsk.se>
2026-05-20 11:53:56 +0200

    jifewjer3

---
+++ Uppgift 10 - Enarmad bandit/uppgift 11 - Klasser/uppgift 11 - Klasser/Animalclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_11___Klasser
+{
+    internal class Animalclass
+    {
+        public string animalname;
+        public string name;
+        public string legAmount;
+    }
+}
--- Uppgift 10 - Enarmad bandit/uppgift 11 - Klasser/uppgift 11 - Klasser/Program.cs
+++ Uppgift 10 - Enarmad bandit/uppgift 11 - Klasser/uppgift 11 - Klasser/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,26 +11,80 @@ namespace uppgift_11___Klasser
     {
         static void Main(string[] args)
         {
+            int playerchoice;
 
-            List<string> Animalclass;
+
             
+
+
+            List<Animalclass> animallist = new List<Animalclass>();
+
+
+            while (true)
             {
+                Console.WriteLine("Write 1 to add animal. Write 2 to see list. Write 3 to quit");
+
+                playerchoice = int.Parse(Console.ReadLine());
+                if (playerchoice == 1)
+                {
 
 
-                Console.WriteLine("please enter household pets");
 
-                Console.ReadLine();
-                
-                Animalclass AnimalInstance;
+                    Console.WriteLine("please enter 1 household pet");
 
+
+
+                    Animalclass AnimalInstance = new Animalclass();
+
+                    AnimalInstance.name = Console.ReadLine();
+
+                    Console.WriteLine("now enter how many legs does a " + AnimalInstance.name + " have");
+
+
+
+                    AnimalInstance.legAmount = Console.ReadLine();
+
+                    Console.WriteLine("a " + AnimalInstance.name + " has " + AnimalInstance.legAmount + " legs! Would you like to add more animals?");
+
+
+
+                    animallist.Add(AnimalInstance);
+
+
+
+                    
+
+
+
+                }
+
+                Console.WriteLine(animallist[0].animalname);
+                if (playerchoice == 2)
+                {
+
+                    for (int i = 0; i < animallist.Count; i++)
+                    {
+                        Console.WriteLine("The " + animallist[i].name + " has " + animallist[i].legAmount + " legs");
+                        
+                    }
+
+                }
+
+
+                if (playerchoice == 3)
+                {
+                    break;
+                }
 
 
 
             }
-
-
-            
         }
+
+
+           
+            
+    }
 
 
             
@@ -40,5 +95,5 @@ namespace uppgift_11___Klasser
 
 
         
-    }
 }
+
