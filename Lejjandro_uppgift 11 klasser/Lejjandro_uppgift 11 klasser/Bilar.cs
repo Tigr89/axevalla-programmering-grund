60797d6916484acef04e9a452b91cdf72a644e62
rymd-spel <ht24lego@axevalla.fhsk.se>
2026-05-20 11:58:35 +0200

    20/05/2026

---
+++ Lejjandro_uppgift 11 klasser/Lejjandro_uppgift 11 klasser/Bilar.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lejjandro_uppgift_11_klasser
+{
+    internal class Bilar
+    {
+        public string bilMärke;
+        public string bilModell;
+        public int bilHastighet;
+        public int bilPris;
+
+        public Bilar(string _märke, string _modell, int _hastighet, int _pris)
+        {
+            bilMärke = _märke;
+            bilModell = _modell;
+            bilHastighet = _hastighet;
+            bilPris = _pris;
+        }
+
+        public int checkSaldo(int saldo)
+        {
+            if (saldo < bilPris)
+            {
+                Console.WriteLine("du har inte råd för den här bilen finns det ett annan bilen som du gillade?");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("Du köpte " + bilMärke + " " + bilModell + ". Grattis och tack så mycket");
+                return bilPris;
+            }
+        }
+
+        public void addCar()
+        {
+            Console.WriteLine("Snälla skriv bil märke");
+            bilMärke = Console.ReadLine();
+            Console.WriteLine("Snälla skriv bil moddel");
+            bilModell = Console.ReadLine();
+            Console.WriteLine("Snälla skriv bil hastighet");
+            bilHastighet = checkToValue(Console.ReadLine());
+            Console.WriteLine("Snälla skriv bil pris");
+            bilPris = checkToValue(Console.ReadLine());
+        }
+
+        int checkToValue(string valueToCheck)
+        {
+            int returnValue;
+
+            while (int.TryParse(valueToCheck, out int number) == false)
+            {
+                Console.WriteLine("ERRO!! Snälla använd ett nummer");
+                valueToCheck = Console.ReadLine();
+            }
+
+            returnValue = int.Parse(valueToCheck);
+
+            return returnValue;
+        }
+    }
+}
---
+++ Lejjandro_uppgift 11 klasser/Lejjandro_uppgift 11 klasser/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lejjandro_uppgift_11_klasser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bilar> bilLista = new List<Bilar>();
+            bilLista.Add(new Bilar("Volvo", "XC60", 180, 570000));
+            bilLista.Add(new Bilar("Tesla", "Model Y", 250, 500000));
+            bilLista.Add(new Bilar("Lamborghini", "Urus", 310, 3000000));
+
+            int saldo = 4000000;
+
+            Console.WriteLine("Välkommer till vår bilhandlare");
+            Console.ReadLine();
+            Console.WriteLine("Vi har många olika typer av bil märker och modeller, jag hoppas att vi har en som du ska gillar");
+            Console.ReadLine();
+            Console.WriteLine("Jag kan se här att du har " + saldo + "kr, det är väldigt mycket pengar");
+            Console.ReadLine();
+            Console.WriteLine("Komm med mig jag ska vissa dig alla billar vi har så du han välja en eller fler som du gillar. Du kan också lägga till bilar om du vill");
+            Console.ReadLine();
+            gamePlay();
+
+            void gamePlay()
+            {
+                while (saldo > 0)
+                {
+                    userAddCar();
+                    
+                    Console.WriteLine("Jag kan se här att du har " + saldo + "kr, kvar vill du köpa mer? If not type: 0");
+
+                    for (int i = 0; i < bilLista.Count; i++)
+                    {
+                        Console.WriteLine((i + 1) + ": " + bilLista[i].bilMärke + " " + bilLista[i].bilModell + " " + bilLista[i].bilHastighet + "km " + bilLista[i].bilPris);
+                    }
+                        Console.WriteLine("0: gå ut/add ny bil");
+
+                    int val = checkToValue(Console.ReadLine());
+
+                    while (val < 0 || val > bilLista.Count)
+                    {
+                        Console.WriteLine("ERRO, Den bil finns inte");
+                        val = checkToValue(Console.ReadLine());
+                    }
+
+                    if (val == 0)
+                    {
+                        userAddCar();
+                        break;
+                    }
+                    else
+                    {
+                        saldo = saldo - bilLista[val - 1].checkSaldo(saldo);
+                    }
+                }
+            }
+
+            void userAddCar()
+            {
+                Console.WriteLine("Vill du lägga till bilar\n1: Ja\n2: Nej\n3: gå ut");
+                int val1 = checkToValue(Console.ReadLine());
+
+                if (val1 == 1)
+                {
+                    Console.WriteLine("Du behöver skriva märke, modell, hastighet och pris av bilen du vill lägga till");
+                    Bilar biladd = new Bilar("","",0,0);
+                    biladd.addCar();
+                    bilLista.Add(biladd);
+                }
+                if (val1 == 3)
+                {
+                    gamePayEnd();
+                }
+            }
+
+            void gamePayEnd()
+            {
+                //Console.Clear();
+                Console.WriteLine("Tack att du har kommit, ha en bra dag och hej då");
+                Console.ReadLine();
+            }
+
+            int checkToValue(string valueToCheck)
+            {
+                int returnValue;
+
+                while (int.TryParse(valueToCheck, out int number) == false)
+                {
+                    Console.WriteLine("ERRO!! Snälla använd ett nummer");
+                    valueToCheck = Console.ReadLine();
+                }
+
+                returnValue = int.Parse(valueToCheck);
+
+                return returnValue;
+            }
+        }
+    }
+}
