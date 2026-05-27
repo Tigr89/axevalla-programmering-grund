614d325653fa1a85a5115b72155963be9fcadadb
Pancake <sspel25adni@axevalla.fhsk.se>
2026-05-27 11:54:18 +0200

    kowefkope

--- Uppgift 12 - Skriva till- och från fil/FileManagement/Program.cs
+++ Uppgift 12 - Skriva till- och från fil/FileManagement/Program.cs
@@ -12,20 +12,36 @@ namespace FileManagement
         static void Main(string[] args)
         {
             //Hämta information ifrån textfilen! Denna fil finns lokalt på datorn.
-            string input01 = File.ReadAllText("../../Tim's mapp/File00.txt");
-            //string input02 = ??? (ReadAllText)
-            //string[] input03 = ??? (ReadAllLines)
-            //string[] input04 = ??? (ReadAllLines)
+            string input01 = File.ReadAllText("../../FilesToRead/File01.txt");
+            string input02 = File.ReadAllText("../../FilesToRead/File02.txt");
+            string[] input03 = File.ReadAllLines("../../FilesToRead/File03.txt");
+           string[] input04 = File.ReadAllLines("../../FilesToRead/File04.txt");
 
             //Jobba med File01 här
             string result01 = File01Task(input01);
             Console.WriteLine(result01);
 
+
+            Console.WriteLine();
+
+
             //Jobba med File02 här
-            //Kalla på funktion
+            List <int> result02 = File02Task(input02);
+            foreach(int number in result02)
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+            Console.WriteLine();
 
             //Jobba med File03 här
-            //Kalla på funktion
+            int[] result03 = File03Task(input03);
+            foreach (int number in result03)
+            {
+                Console.WriteLine(number);
+            }
 
             //Jobba med File04 här
             //Kalla på funktion
@@ -42,14 +58,18 @@ namespace FileManagement
             //Detta så att vi kan använda oss av funktionen "sort".
             char[] listToSort = input.ToCharArray();
             string returnString = "";
+            
 
-            foreach(char letter in listToSort)
+            //sortera arrayen här.
+            Array.Sort(listToSort);
+
+
+            foreach (char letter in listToSort)
             {
                 returnString += letter;
             }
 
-            //sortera arrayen här.
-            //???
+            
 
             //Här skickar vi tillbaka listan i datatypen "string".
             return returnString;
@@ -68,23 +88,33 @@ namespace FileManagement
             {
                 //För varje (for each) siffra (vårt valda variabelnamn) i vår lista
                 //ska läggas in i vår lista "intsInList". 
+                intsInList.Add(int.Parse(siffra));
             }
+            intsInList.Sort();
 
             return intsInList;
         }
 
         static int[] File03Task(string[] input)
         {
+
+
             //Här får ni en färdig array med rader som enbart består av "bokstavssiffror".
             //Ert uppdrag är att sortera arrayen och skicka tillbaka den till main.
+            
 
             //Börja med att konvertera varje array element till en siffra och spara i en List<int>.
-
+            List<int> numberList = new List<int>();
             //Sortera listan!
+            foreach(string number in input)
+            {
+                numberList.Add(int.Parse(number));
+            }
 
+            numberList.Sort();
             //Returnera listan!
             //Ersätt null med ert resultat!
-            return null;
+            return numberList.ToArray();
         }
 
         static void File04Task(string[] input)
