091670b47e583880dbe62bbdbadeaa0b123ec14f
Tigr89 <tim@axevalla.fhsk.se>
2026-05-27 10:52:04 +0200

    WoW_exempel

---
+++ Övrigt/WoW_Ability_Breakdown/WoW_Ability_Breakdown/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoW_Ability_Breakdown
+{
+    abstract class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int cost;
+            float cooldown;
+
+        }
+    }
+}
---
+++ Övrigt/WoW_Ability_Breakdown/WoW_Ability_Breakdown/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WoW_Ability_Breakdown")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WoW_Ability_Breakdown")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3579bed7-144c-4dc1-9ea7-4cc632dc7f26")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
